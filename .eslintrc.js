56a630c0c90e1bcaab381ed42a2ef3f91d2b180e
Chang Wang <garnwraly@gmail.com>
2017-12-01 11:24:07 -0500

    initial gatsby setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ["react-app", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": [1, { trailingComma: "all", singleQuote: true }]
+    }
+  };
+  
