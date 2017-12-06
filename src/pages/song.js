9a65e50c9c7849292352fdb2a39f98dfa62734c2
Chang Wang <garnwraly@gmail.com>
2017-12-06 16:17:53 -0500

    key-points

--- src/pages/song.js
+++ src/pages/song.js
@@ -5,6 +5,8 @@ import styled, { css } from 'react-emotion';
 import { container, slimContainer } from 'common/layout';
 import colors from 'common/colors';
 import Header from 'components/Header';
+import KeyPoint from 'components/product/KeyPoint';
+import SecondaryPoint from 'components/product/SecondaryPoint';
 
 const Song = () => (
   <React.Fragment>
@@ -88,6 +90,38 @@ const Song = () => (
           />
         </div>
       </div>
+    </div>
+    <div
+      className={
+        `key-points` +
+        css`
+          ${container};
+          display: flex;
+          margin-top: 150px;
+          margin-bottom: 150px;
+          .key-point {
+            flex-basis: calc(100% / 3);
+          }
+        `
+      }
+    >
+      <KeyPoint
+        iconSrc={require('assets/icons/bars.svg')}
+        title={`Scalable`}
+        body={`Designed to handle the volume of your requests in an efficient and timely manner.`}
+      />
+
+      <KeyPoint
+        iconSrc={require('assets/icons/download.svg')}
+        title={`Easy to adopt`}
+        body={`Relying on a standard REST API. Get started running SONG with  two Docker commands.`}
+      />
+
+      <KeyPoint
+        iconSrc={require('assets/icons/checkmark.svg')}
+        title={`Accurate and Efficient`}
+        body={`Specifically designed to track genome data, SONG tracks and validates your submissions.`}
+      />
     </div>
   </React.Fragment>
 );
