724568b8551d77e7782266ccd985bde851676ce8
teesloane <tylersloane@gmail.com>
2018-05-13 18:55:29 -0400

    Add jira embed

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export default class HTML extends Component {
+
   render() {
     let css
     if (process.env.NODE_ENV === 'production') {
@@ -47,6 +48,10 @@ export default class HTML extends Component {
           />
           {this.props.postBodyComponents}
           <script src={__PATH_PREFIX__ + '/js/toggle.js'} />
+          
+          {/* JIRA EMBED SCRIPT */}
+          <script src={__PATH_PREFIX__ + '/js/jira_embed.js'} />
+          <script type='text/javascript' src='https://extsd.oicr.on.ca/s/5hcqv5/75007/b6b48b2829824b869586ac216d119363/1.1.4.1/_/download/resources/com.jelldesk.apps.embeddable-widget:widget-client-resources/embeddable-client.js'></script>
         </body>
       </html>
     )
