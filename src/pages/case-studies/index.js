3612e4eeca9d27134dfc367b3dcd28472bca6bce
teesloane <tylersloane@gmail.com>
2019-03-04 19:14:35 -0500

    add waypoint.

--- src/pages/case-studies/index.js
+++ src/pages/case-studies/index.js
@@ -1,13 +1,13 @@
 /**
  * This component represents the `Case Studies` page.
- * The page is made up of 
-  * a) several case studies and;
-  * b) a navigation component for scrolling / jumping between different case studies.
-  * NOTE: The structure of a case studies is laregly the same, so we render
-  * the UI based on a data structure in ./data.js 
-  * This component is a Class component because 
-    * a) each case study has a "slider" for displaying screenshots
-    * b) for indicating the currently selected logo in the navigation.
+ * The page is made up of
+ * a) several case studies and;
+ * b) a navigation component for scrolling / jumping between different case studies.
+ * NOTE: The structure of a case studies is laregly the same, so we render
+ * the UI based on a data structure in ./data.js
+ * This component is a Class component because
+ * a) each case study has a "slider" for displaying screenshots
+ * b) for indicating the currently selected logo in the navigation.
  */
 
 import React, { Component } from "react";
@@ -16,8 +16,25 @@ import CaseStudy from "./case-study";
 import caseData from "./data";
 import Navigation from "./navigation";
 import "./styles.scss";
+import { Waypoint } from "react-waypoint";
 
 class CaseStudiesPage extends Component {
+
+  state = {
+    currentCase: null
+  }
+
+  _handleWaypointEnter(caseStudy, e) {
+    console.log("entered area", caseStudy);
+    this.setState({currentCase: caseStudy.title})
+  }
+
+  _handleWaypointLeave(caseStudy, e) {
+    console.log("left area", caseStudy);
+    // this.setState({currentCase: caseStudy.title})
+  }
+
+
   render() {
     return (
       <Layout>
@@ -35,19 +52,26 @@ class CaseStudiesPage extends Component {
           </section>
 
           {/* Case Study Interactive NavBar */}
-          <Navigation />
+          <Navigation currentCase={this.state.currentCase} />
 
           {/* Case Study Component */}
           {caseData.map(d => {
             return (
-              <CaseStudy
-                title={d.title}
-                description={{ __html: d.description }}
-                clientListItem={d.listItems}
-                clientLink={d.checkItOutLink}
-                logo={d.logo}
-                details={d.details}
-              />
+              <Waypoint
+                onEnter={(e) => this._handleWaypointEnter(d, e)}
+                onLeave={(e) => this._handleWaypointLeave(d, e)}
+              >
+                <div>
+                  <CaseStudy
+                    title={d.title}
+                    description={{ __html: d.description }}
+                    clientListItem={d.listItems}
+                    clientLink={d.checkItOutLink}
+                    logo={d.logo}
+                    details={d.details}
+                  />
+                </div>
+              </Waypoint>
             );
           })}
         </main>
--- src/pages/case-studies/navigation.js
+++ src/pages/case-studies/navigation.js
@@ -3,19 +3,21 @@ import React from "react";
 // Logos
 import caseData from "./data";
 
-const Navigation = () => {
+const Navigation = ({currentCase}) => {
   return (
     <div className="CaseStudies-Navigation">
       {caseData.map((d, i) => (
-        <NavigationItem key={i} data={d} />
+        <NavigationItem currentCase={currentCase} key={i} data={d} />
       ))}
     </div>
   );
 };
 
-const NavigationItem = ({ data }) => {
+const NavigationItem = ({ data, currentCase }) => {
+  let active = currentCase == data.title ? "active" : "";
+
   return (
-    <div className="nav-item-border">
+    <div className={`nav-item-border ${active}`}>
       <img alt={data.title} className="nav-item" src={data.logo} />
     </div>
   );
