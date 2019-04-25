c368366ec00b254c6c7f7025070a10238733d4a5
teesloane <tylersloane@gmail.com>
2019-04-25 16:05:52 -0400

    add links to case study descriptions.

--- src/pages/case-studies/data.js
+++ src/pages/case-studies/data.js
@@ -6,6 +6,7 @@ import gdc_sapien from './assets/gdc/sapien.png'
 import gdc_oncogrid from './assets/gdc/oncogrid.png'
 import gdc_survival from './assets/gdc/survival.png'
 import gdc_logo from './assets/gdc/logo.svg'
+const gdc_link = "https://portal.gdc.cancer.gov";
 
 // Kids First Assets
 import kf_arranger from './assets/kidsfirst/arranger.png'
@@ -13,14 +14,17 @@ import kf_ego from './assets/kidsfirst/ego.png'
 import kf_persona from './assets/kidsfirst/persona.png'
 import kf_riff from './assets/kidsfirst/riff.png'
 import kf_logo from "./assets/kidsfirst/logo.svg" 
+const kf_link = "https://portal.kidsfirstdrc.org"
 
 // ICGC Assets -- TODO Pending Screenshots
 import icgc_onco from './assets/icgc/onco.png';
 import icgc_logo from "./assets/icgc/logo.svg";
+const icgc_link = "https://dcc.icgc.org";
 
 // CGC Assets -- TODO Pending Screenshots
 import cgc_logo from "./assets/cgc/logo.svg";
 import screenCGC from "./assets/cgc/screenshot-cancer-collaboratory-temp.png";
+const cgc_link = "https://cancercollaboratory.org";
 
 // HCMIS -- TODO Pending Screenshots
 import hcmis_logo from './assets/hcmis/logo.svg';
@@ -30,7 +34,7 @@ export default [
   {
     title: "Kids First Data Resource Center",
     slug: "kidsFirst",
-    description: "The Kids First Data Resource Center brings together clinical and genetic data from pediatric cancer and structural birth defect cohorts into a centralized, cloud-based discovery portal. We created a collaborative, community focused portal that brings together researchers, health professionals, and patients to accelerate discoveries that improve the lives of pediatric patients and their families.",
+    description: `The <a target='_blank' href='${kf_link}'>Kids First Data Resource Center </a> brings together clinical and genetic data from pediatric cancer and structural birth defect cohorts into a centralized, cloud-based discovery portal. We created a collaborative, community focused portal that brings together researchers, health professionals, and patients to accelerate discoveries that improve the lives of pediatric patients and their families.`,
     logo: kf_logo,
     listItems: [
       "Integration with the cloud workflow resource, Cavatica",
@@ -38,7 +42,7 @@ export default [
       "Innovative data searching and visualization",
       "Interactive cohort builder allowing query combinations",
     ],
-    clientLink: "portal.kidsfirstdrc.org",
+    clientLink: kf_link,
     details: [
       {
         title: "Arranger",
@@ -65,14 +69,14 @@ export default [
   {
     title: "ICGC Data Portal",
     slug: "icgcDataPortal",
-    description: "The International Cancer Genome Consortium (ICGC) is a global initiative to build a comprehensive catalog of mutational abnormalities in the major tumor types. The portal currently contains data from 86 worldwide cancer projects, collectively representing about molecular data from over 22,000 donors with more than 80 million somatic mutations accessible to researchers.",
+    description: `The <a target='_blank' href='${icgc_link}'>International Cancer Genome Consortium (ICGC) </a> is a global initiative to build a comprehensive catalog of mutational abnormalities in the major tumor types. The portal currently contains data from 86 worldwide cancer projects, collectively representing about molecular data from over 22,000 donors with more than 80 million somatic mutations accessible to researchers.`,
     logo: icgc_logo,
     listItems: [
       "Interoperability with distributed data repositories including AWS S3 and Cancer Genome Collaboratory",
       "Beautiful and informative genomic visualizations",
       "In-browser analysis on cancer genomic data",
     ],
-    clientLink: "https://dcc.icgc.org",
+    clientLink: icgc_link,
     details: [
       {
         title: "Score",
@@ -107,36 +111,41 @@ export default [
     title: "NCI GDC Data Portal",
     slug: "nciGdc",
     logo: gdc_logo,
-    description: "The National Cancer Institute’s GDC Data Portal has harmonized genomic data from over 33,000 cancer patients from programs such as The Cancer Genome Atlas (TCGA), Therapeutically Applicable Research to Generate Effective Treatments (TARGET) and Foundation Medicine. ",
+    description: `The <a href='${gdc_link}' target='_blank'>National Cancer Institute’s GDC Data Portal</a> has harmonized genomic data from over 33,000 cancer patients from programs such as The Cancer Genome Atlas (TCGA), Therapeutically Applicable Research to Generate Effective Treatments (TARGET) and Foundation Medicine.`,
     listItems: [
       "Explore genomic and clinical data from cancer patients",
       "Create virtual cohort by search across all patients for specific clinical attributes and genomic mutations",
     ],
-    clientLink: "https://portal.gdc.cancer.gov",
+    clientLink: gdc_link,
     details: [
       {
-        title: "Billing & Usage",
-        description: "Track and manage compute usage on an Openstack tenant to streamline customer billing operations.",
+        title: "OncoJS",
+        description: "Sapien correlates donors to primary sites of the human body.",
         screenshot: gdc_sapien
       },
       {
-        title: "Enrollment",
-        description: "Automatically manage user enrolment to a project, as well as supporting user self-registration.",
+        title: "OncoJS",
+        description: "OncoGrid visualizes genomic data to identify trends and patterns.",
         screenshot: gdc_oncogrid
+      },
+      {
+        title: "OncoJS",
+        description: "Survivalplot visualizes datasets to determine probable health outcomes.",
+        screenshot: gdc_survival
       },
     ]
   },
   {
     title: "Cancer Genome Collaboratory",
     slug: "cgc",
-    description: "The Cancer Genome Collaboratory is an academic compute cloud resource that allows researchers to run complex analysis operations across large ICGC cancer genome data sets as well as their own data. One petabyte of ICGC data is co-located with elastic compute enabling researchers to bring analysis tools to the data for faster discovery.",
+    description: `The <a target='_blank' href='${cgc_link}'>Cancer Genome Collaboratory</a> is an academic compute cloud resource that allows researchers to run complex analysis operations across large ICGC cancer genome data sets as well as their own data. One petabyte of ICGC data is co-located with elastic compute enabling researchers to bring analysis tools to the data for faster discovery.`,
     logo: cgc_logo,
     listItems: [
       "Self-service cloud computing made affordable",
       "Convenient usage tracking for Openstack ",
       "Speed research up by bringing analysis to the data",
     ],
-    clientLink: "https://cancercollaboratory.org",
+    clientLink: cgc_link,
     details: [
       {
         title: "Billing & Usage",
@@ -154,13 +163,13 @@ export default [
   {
     title: "Human Cancer Models Initiative",
     slug: "humanCancerModels",
-    description: "The Human Cancer Models Initiative Searchable Catalog is an interactive catalog of purchasable next-generation, tumor-derived organoids and cell lines. The catalog is supported by a custom content management system for entry and validation of clinical and genomic data. Investigate individual model pages for licensing requirements, molecular features, culturing requirements and purchasing details. ",
+    description: `The <a href='${hcmis_link}' target='_blank'>Human Cancer Models Initiative Searchable Catalog</a> is an interactive catalog of purchasable next-generation, tumor-derived organoids and cell lines. The catalog is supported by a custom content management system for entry and validation of clinical and genomic data. Investigate individual model pages for licensing requirements, molecular features, culturing requirements and purchasing details. `,
     logo: hcmis_logo,
     listItems: [
       "Customized “shopping” experience for researchers to browse innovative cancer models",
       "Models annotated with genomic and clinical data",
     ],
-    clientLink: "https://hcmi-searchable-catalog.nci.nih.gov",
+    clientLink: hcmis_link,
     details: [
       {
         title: "Arranger",
