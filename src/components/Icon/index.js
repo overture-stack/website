6b10d9b28fda0ba0a3fc5af4747c364cea0137f8
teesloane <tylersloane@gmail.com>
2019-08-19 11:55:56 -0400

    Continue product box layout.

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,43 +1,52 @@
 // All them icons
 import React from 'react'
 import './styles.scss'
-import arrowRight from './icons/arrow_right.svg'
+import arrowRight        from './icons/arrow_right.svg'
 import arrowRightMagenta from './icons/arrow_right_magenta.svg'
-import arrowDown from './icons/arrow_down.svg'
-import barGraph from './icons/bar_graph.svg'
-import cardStack from './icons/card_stack.svg'
-import checkmark from './icons/checkmark.svg'
-import cloud from './icons/cloud.svg'
-import cloudSquare from './icons/cloud_square.svg'
-import database from './icons/database.svg'
-import dna from './icons/dna.svg'
-import download from './icons/download.svg'
-import extensible from './icons/extensible.svg'
-import fingerSnap from './icons/finger_snap.svg'
-import githubGrey from './icons/github_grey.svg'
-import githubWhite from './icons/github_white.svg'
-import githubYellow from './icons/github_yellow.svg'
-import graphCycle from './icons/graph_cycle.svg'
-import gridGlass from './icons/grid_magnifying_glass.svg';
-import history from './icons/history.svg'
-import lock from './icons/lock.svg'
-import lockCode from './icons/lock_code.svg'
-import mail from './icons/mail.png'
-import magnify from './icons/magnify.svg'
-import pageWhite from './icons/page_white.svg'
-import palette from './icons/palette.svg'
-import power from './icons/power.svg'
-import productScore from './icons/productScore.svg'
-import productSong from './icons/productSong.svg'
-import rocketWhite from './icons/rocket_white.svg'
-import security from './icons/security.svg'
-import share from './icons/share.svg'
-import shield from './icons/shield.svg'
-import spiral from './icons/spiral.svg'
-import star from './icons/star.svg'
-import target from './icons/target.svg'
-import user from './icons/user.svg'
-import slack from './icons/icon-slack.svg'
+import arrowDown         from './icons/arrow_down.svg'
+import barGraph          from './icons/bar_graph.svg'
+import cardStack         from './icons/card_stack.svg'
+import checkmark         from './icons/checkmark.svg'
+import cloud             from './icons/cloud.svg'
+import cloudSquare       from './icons/cloud_square.svg'
+import database          from './icons/database.svg'
+import dna               from './icons/dna.svg'
+import download          from './icons/download.svg'
+import extensible        from './icons/extensible.svg'
+import fingerSnap        from './icons/finger_snap.svg'
+import githubGrey        from './icons/github_grey.svg'
+import githubWhite       from './icons/github_white.svg'
+import githubYellow      from './icons/github_yellow.svg'
+import graphCycle        from './icons/graph_cycle.svg'
+import gridGlass         from './icons/grid_magnifying_glass.svg';
+import history           from './icons/history.svg'
+import lock              from './icons/lock.svg'
+import lockCode          from './icons/lock_code.svg'
+import mail              from './icons/mail.png'
+import magnify           from './icons/magnify.svg'
+import pageWhite         from './icons/page_white.svg'
+import palette           from './icons/palette.svg'
+import power             from './icons/power.svg'
+import productScore      from './icons/productScore.svg'
+import productSong       from './icons/productSong.svg'
+import productPersona    from './icons/productPersona.svg'
+import productArranger   from './icons/productArranger.svg'
+import productMaestro    from './icons/productMaestro.svg'
+import productEgo        from './icons/productEgo.svg'
+import productOnco       from './icons/productOnco.svg'
+import productRiff       from './icons/productRiff.svg'
+import productEnrolment  from './icons/productEnrolment.svg'
+import productJukebox    from './icons/productJukebox.svg'
+import productBilling    from './icons/productBilling.svg'
+import rocketWhite       from './icons/rocket_white.svg'
+import security          from './icons/security.svg'
+import share             from './icons/share.svg'
+import shield            from './icons/shield.svg'
+import spiral            from './icons/spiral.svg'
+import star              from './icons/star.svg'
+import target            from './icons/target.svg'
+import user              from './icons/user.svg'
+import slack             from './icons/icon-slack.svg'
 
 const icons = {
   arrowRight,
@@ -65,8 +74,18 @@ const icons = {
   magnify,
   pageWhite,
   palette,
-  productScore,
   productSong,
+  productScore,
+  productPersona,
+  productArranger,
+  productMaestro,
+  productEgo,
+  productOnco,
+  productRiff,
+  productMaestro,
+  productJukebox,
+  productBilling,
+  productEnrolment,
   power,
   rocketWhite,
   security,
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import heroImg from "../about-us/assets/hero_img.svg";
+import { Link } from "gatsby";
 import {
   H1,
   H2,
@@ -16,12 +17,20 @@ import {
 } from "../../components";
 import "./style.scss";
 
-
-const ProductBox = ({ header, details, links, isCore = false, }) => {
+const ProductBox = ({
+  header,
+  headerColor,
+  containerClass = "is-6",
+  bgColor = "#fff",
+  details,
+  links,
+  isCore = false,
+  iconSize = 32
+}) => {
   return (
-    <div className="product-box">
-      {isCore && (<IconCommon.Core/>)}
-      <div className="heading">
+    <div className={`product-box column ${containerClass}`} style={{backgroundColor: bgColor}}>
+      {isCore && <IconCommon.Core />}
+      <div className="heading" style={{ color: headerColor }}>
         {header}
       </div>
       <div className="details">{details}</div>
@@ -29,29 +38,99 @@ const ProductBox = ({ header, details, links, isCore = false, }) => {
         {links.map(l => {
           return (
             <span className="icon-link">
-              <Icon className="icon" size={32} img={l.icon} ></Icon>
-              <a className="link" href={l.link}>{l.text}</a>
+              <Link className="link" to={`/products/${l.link}`}>
+                <Icon
+                  className="icon"
+                  size={l.iconSize ? l.iconSize : iconSize}
+                  img={l.icon}
+                ></Icon>
+                {l.text}
+              </Link>
             </span>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
 const featureGridData = [
   [
     {
       header: "Generate & Upload",
+      headerColor: "#D087B4",
+      bgColor: "#F2F3F5",
       isCore: true,
-      details: "Products that allow your data submitters to generate genomic and clinical metadata and securely upload the corresponding files: ",
-      links: [{ icon: "productScore", text: "Score", link: "/" }, { icon: "productSong", text: "Song", link: "/" }],
-      ChildComponent: ProductBox,
+      details:
+        "Products that allow your data submitters to generate genomic and clinical metadata and securely upload the corresponding files: ",
+      links: [
+        { icon: "productScore", text: "Score", link: "score" },
+        { icon: "productSong", text: "Song", link: "song" }
+      ],
+      ChildComponent: ProductBox
     },
+
+    {
+      header: "Access & Download",
+      headerColor: "#6AC3E8",
+      bgColor: "#F2F3F5",
+      isCore: true,
+      details:
+        "Products that give your users the correct permissions to view and download controlled data: ",
+      links: [
+        { icon: "productEgo", text: "Ego", link: "ego", iconSize: 24 },
+        {
+          icon: "productMaestro",
+          text: "Maestro",
+          link: "maestro",
+          iconSize: 40
+        },
+        { icon: "productArranger", text: "Arranger", link: "arranger" }
+      ],
+      ChildComponent: ProductBox
+    }
   ],
-]
+  [
+    {
+      header: "Analyze & Discover",
+      headerColor: "#C86370",
+      details:
+        "Products that allow your users to access interactive visualizations and code-based analysis environments: ",
+      links: [
+        { icon: "productJukebox", text: "Jukebox", link: "jukebox" },
+        { icon: "productOnco", text: "OncoJS", link: "oncojs" }
+      ],
+      ChildComponent: ProductBox
+    },
 
+    {
+      header: "Collaborate & Share",
+      headerColor: "#47D9BF",
+      details:
+        "Products that allow your users to set up a profile and share urls in order to  raise awareness across the scientific community: ",
+      links: [
+        { icon: "productPersona", text: "Persona", link: "persona" },
+        { icon: "productRiff", text: "Riff", link: "riff" }
+      ],
+      ChildComponent: ProductBox
+    }
+  ],
 
+  [
+    {
+      header: "Track & Manage",
+      headerColor: "#ECC000",
+      containerClass: "is-6 is-offset-3",
+      details:
+        "Products that help you track and manage projects, users, billing and resource usage for an OpenStack cloud environment: ",
+      links: [
+        { icon: "productBilling", text: "Billing & Usage", link: "" }, // TODO: need real links here.
+        { icon: "productEnrolment", text: "Enrolment", link: "" }      // TODO: needs a real link here.
+      ],
+      ChildComponent: ProductBox
+    }
+  ]
+];
 
 class ProductsPage extends Component {
   render() {
@@ -65,13 +144,16 @@ class ProductsPage extends Component {
             subtitle="Build your own genomics platform that allows your users to collaborate and share their scientific discoveries."
             fgImage="img_products"
             fgImageClass="img_hero_products"
-          >
-          </Hero>
+          ></Hero>
 
           <div className="spacer" />
 
           {/* Grid sections */}
-          <GridFeature iconSize={48} data={featureGridData} childComponent={ProductBox} />
+          <GridFeature
+            iconSize={48}
+            data={featureGridData}
+            childComponent={ProductBox}
+          />
 
           <BottomCallout>
             <Callout
