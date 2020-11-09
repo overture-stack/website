8d8b677793253d2301bfcb7e24ec5cd08bc886c0
Sam Rich <samrichca@users.noreply.github.com>
2020-11-09 12:32:47 -0500

    add icon to anchor headings

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,19 +3,14 @@ import { Layout } from 'components';
 import { MDXProvider } from '@mdx-js/react';
 import { AnchorHeading, WarningBox as Warning } from 'components';
 
-const headingsMax = 6;
-const limitHeadings = n => (n > headingsMax ? headingsMax : n);
-const headings = [...Array(headingsMax)]
-  // h1 becomes h2-with-anchor-tag, etc up to h6 (edge case, h6 is rarely used)
-  .reduce(
-    (acc, curr, i) => ({
-      ...acc,
-      [`h${limitHeadings(i + 1)}`]: ({ children }) => (
-        <AnchorHeading priority={limitHeadings(i + 2)}>boop {children}</AnchorHeading>
-      ),
-    }),
-    {}
-  );
+const headings = {
+  h1: props => <AnchorHeading size="h2" {...props} />,
+  h2: props => <AnchorHeading size="h3" {...props} />,
+  h3: props => <AnchorHeading size="h4" {...props} />,
+  h4: props => <AnchorHeading size="h5" {...props} />,
+  h5: props => <AnchorHeading size="h6" {...props} />,
+  h6: props => <AnchorHeading size="h6" {...props} />,
+};
 
 const shortcodes = { Warning };
 
--- src/components/AnchorHeading/index.js
+++ src/components/AnchorHeading/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Icon } from 'components';
+import { Link } from 'gatsby';
+import GithubSlugger from 'github-slugger';
+import { GoLink as LinkIcon } from 'react-icons/go';
 
-export default function AnchorHeading({ children, priority }) {
-  const CustomTag = `h${priority}`;
+const slugger = new GithubSlugger();
 
+export default function AnchorHeading({ children, size: H, ...props }) {
+  const slug = typeof children === 'string' ? slugger.slug(children) : slugger.slug(children[0]);
+  // children is an array if some of the heading text
+  // is formatted, e.g. bold or italic
   return (
-    <CustomTag>
-      <a href="" className="anchor before">
-        <Icon img="arrowRightMagenta" size={12} />
-      </a>
+    <H {...props}>
+      <Link id={slug} to={`#${slug}`}>
+        <LinkIcon size="16" />
+      </Link>
       {children}
-    </CustomTag>
+    </H>
   );
 }
