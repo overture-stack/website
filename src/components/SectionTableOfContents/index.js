775f716ea07436326d7ce8a618914a0e9ed4c05c
Sam Rich <samrichca@users.noreply.github.com>
2020-11-30 16:48:28 -0500

    change link from heading to first page of section

--- src/components/SectionTableOfContents/index.js
+++ src/components/SectionTableOfContents/index.js
@@ -16,8 +16,8 @@ const makeUrl = url => `/documentation/${url}/`;
 const RenderItems = ({ isMenuActive = false, pages, path }) => (
   <ol className={isMenuActive ? 'menu-active' : ''}>
     {pages.map(page => {
-      const url = makeUrl(page.url);
       const { isHeading = false, pages, title } = page;
+      const url = makeUrl(page.url);
       const isLinkActive = path === url;
       const isNextMenuActive = pages && path.includes(url);
       const className = `${isLinkActive ? 'link-active' : ''} ${
@@ -25,9 +25,12 @@ const RenderItems = ({ isMenuActive = false, pages, path }) => (
       }`;
       const iconImg = isNextMenuActive ? 'chevronMagenta' : 'chevronGrey';
       const iconStyle = isNextMenuActive ? { transform: 'rotate(90deg)' } : {};
+      // if this link is a heading,
+      // change link to the first page of this section
+      const linkUrl = isHeading && pages ? makeUrl(pages[0].url) : url;
       return (
         <li key={url}>
-          <Link className={className} to={url}>
+          <Link className={className} to={linkUrl}>
             <span>
               {pages && <Icon img={iconImg} size={7} style={iconStyle} />}
               {title}
