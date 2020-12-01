97a319c66fa602454c0ab37cd81f77a9d18249f9
Sam Rich <samrichca@users.noreply.github.com>
2020-12-01 12:03:23 -0500

    sidebar highlight works

--- src/components/SectionTableOfContents/index.js
+++ src/components/SectionTableOfContents/index.js
@@ -13,35 +13,37 @@ import './styles.scss';
 
 const makeUrl = url => `/documentation/${url}/`;
 
-const RenderItems = ({ isMenuActive = false, pages, path }) => (
-  <ol className={isMenuActive ? 'menu-active' : ''}>
-    {pages.map(page => {
-      const { isHeading = false, pages, title } = page;
-      const url = makeUrl(page.url);
-      const isLinkActive = path === url;
-      const isNextMenuActive = pages && path.includes(url);
-      const className = `${isLinkActive ? 'link-active' : ''} ${
-        isNextMenuActive ? 'menu-active__heading' : ''
-      }`;
-      const iconImg = isNextMenuActive ? 'chevronMagenta' : 'chevronGrey';
-      const iconStyle = isNextMenuActive ? { transform: 'rotate(90deg)' } : {};
-      // if this link is a heading,
-      // change link to the first page of this section
-      const linkUrl = isHeading && pages ? makeUrl(pages[0].url) : url;
-      return (
-        <li key={url}>
-          <Link className={className} to={linkUrl}>
-            <span>
-              {pages && <Icon img={iconImg} size={7} style={iconStyle} />}
-              {title}
-            </span>
-          </Link>
-          {pages && <RenderItems isMenuActive={isNextMenuActive} pages={pages} path={path} />}
-        </li>
-      );
-    })}
-  </ol>
-);
+const RenderItems = ({ pages, path }) => {
+  console.log({ path, pages });
+  return (
+    <ol>
+      {pages.map(page => {
+        const { isHeading = false, pages, title } = page;
+        const url = makeUrl(page.url);
+        const isLinkActive = path === url;
+        const isMenuActive = pages && path.includes(url);
+        const liClassName = isMenuActive ? 'menu-active' : '';
+        const aClassName = isLinkActive ? 'link-active' : '';
+        const iconImg = isMenuActive ? 'chevronMagenta' : 'chevronGrey';
+        const iconStyle = isMenuActive ? { transform: 'rotate(90deg)' } : {};
+        // if this link is a heading,
+        // change link to the first page of this section
+        const linkUrl = isHeading && pages ? makeUrl(pages[0].url) : url;
+        return (
+          <li key={url} className={liClassName}>
+            <Link to={linkUrl} className={aClassName}>
+              <span>
+                {pages && <Icon img={iconImg} size={7} style={iconStyle} />}
+                {title}
+              </span>
+            </Link>
+            {pages && isMenuActive && <RenderItems pages={pages} path={path} />}
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
 
 export default function SectionTableOfContents({ pages, path }) {
   return (
