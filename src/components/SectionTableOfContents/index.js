5a6a976bfc7460fe26f6f1a15fc8323076c69ab8
Sam Rich <samrichca@users.noreply.github.com>
2020-12-01 12:10:53 -0500

    refactor section TOC

--- src/components/SectionTableOfContents/index.js
+++ src/components/SectionTableOfContents/index.js
@@ -3,47 +3,40 @@ import { Link } from 'gatsby';
 import { Icon } from 'components';
 import './styles.scss';
 
-// first level: buttons + dropdowns
-// subsequent levels: <Link> tags + dropdowns
-
-// flatten to find partial match??
-
-// if page has pages then make it a button
-// if !isHeading then navigate with reach/router
-
 const makeUrl = url => `/documentation/${url}/`;
 
-const RenderItems = ({ pages, path }) => {
-  console.log({ path, pages });
-  return (
-    <ol>
-      {pages.map(page => {
-        const { isHeading = false, pages, title } = page;
-        const url = makeUrl(page.url);
-        const isLinkActive = path === url;
-        const isMenuActive = pages && path.includes(url);
-        const liClassName = isMenuActive ? 'menu-active' : '';
-        const aClassName = isLinkActive ? 'link-active' : '';
-        const iconImg = isMenuActive ? 'chevronMagenta' : 'chevronGrey';
-        const iconStyle = isMenuActive ? { transform: 'rotate(90deg)' } : {};
-        // if this link is a heading,
-        // change link to the first page of this section
-        const linkUrl = isHeading && pages ? makeUrl(pages[0].url) : url;
-        return (
-          <li key={url} className={liClassName}>
-            <Link to={linkUrl} className={aClassName}>
-              <span>
-                {pages && <Icon img={iconImg} size={7} style={iconStyle} />}
-                {title}
-              </span>
-            </Link>
-            {pages && isMenuActive && <RenderItems pages={pages} path={path} />}
-          </li>
-        );
-      })}
-    </ol>
-  );
-};
+const RenderItems = ({ pages, path }) => (
+  <ol>
+    {pages.map(page => {
+      const { isHeading, pages, title } = page;
+      const url = makeUrl(page.url);
+      const isLinkActive = path === url;
+      const aClassName = isLinkActive ? 'link-active' : '';
+
+      // submenus
+      const isMenuActive = pages && path.includes(url);
+      const liClassName = isMenuActive ? 'menu-active' : '';
+      const iconImg = isMenuActive ? 'chevronMagenta' : 'chevronGrey';
+      const iconStyle = isMenuActive ? { transform: 'rotate(90deg)' } : {};
+
+      // headings
+      // change link destination to first child page
+      const linkUrl = isHeading && pages ? makeUrl(pages[0].url) : url;
+
+      return (
+        <li key={url} className={liClassName}>
+          <Link to={linkUrl} className={aClassName}>
+            <span>
+              {pages && <Icon img={iconImg} size={7} style={iconStyle} />}
+              {title}
+            </span>
+          </Link>
+          {isMenuActive && <RenderItems pages={pages} path={path} />}
+        </li>
+      );
+    })}
+  </ol>
+);
 
 export default function SectionTableOfContents({ pages, path }) {
   return (
