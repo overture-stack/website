48250cb38cb96eaef2b32f023c3bc7bed8026977
Sam Rich <samrichca@users.noreply.github.com>
2020-12-02 11:17:00 -0500

    restrict headings menu to two levels

--- src/components/HeadingsTableOfContents/index.js
+++ src/components/HeadingsTableOfContents/index.js
@@ -13,33 +13,31 @@ const getIds = items =>
     []
   );
 
-const RenderItems = ({ activeId, items, location }) => {
-  return (
-    <ol className="toc-headings__list">
-      {items.map(item => {
-        const linkActive = activeId === item.url.slice(1);
-        const linkClassName = linkActive ? 'active' : '';
-        return (
-          <li key={item.url}>
-            <HashLink className={linkClassName} location={location} to={item.url}>
-              <span>{item.title}</span>
-            </HashLink>
-            {item.items && (
-              <RenderItems activeId={activeId} items={item.items} location={location} />
-            )}
-          </li>
-        );
-      })}
-    </ol>
-  );
-};
+const RenderItems = ({ activeId, hasChildMenu = false, items, location }) => (
+  <ol className="toc-headings__list">
+    {items.map(item => {
+      const linkActive = activeId === item.url.slice(1);
+      const linkClassName = linkActive ? 'active' : '';
+      return (
+        <li key={item.url}>
+          <HashLink className={linkClassName} location={location} to={item.url}>
+            <span>{item.title}</span>
+          </HashLink>
+          {item.items && hasChildMenu && (
+            <RenderItems activeId={activeId} items={item.items} location={location} />
+          )}
+        </li>
+      );
+    })}
+  </ol>
+);
 
 export default function HeadingsTableOfContents({ items, location }) {
   const idList = getIds(items);
   const activeId = useActiveId(idList);
   return (
     <div className="toc-headings">
-      <RenderItems activeId={activeId} items={items} location={location} />
+      <RenderItems activeId={activeId} hasChildMenu items={items} location={location} />
     </div>
   );
 }
