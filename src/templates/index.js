b6726ed4708b91e04ea9d6674e091a34cc3a7d48
Sam Rich <samrichca@users.noreply.github.com>
2023-08-30 14:16:29 -0400

    #275: add matomo tag manager (#304)

--- src/templates/index.js
+++ src/templates/index.js
@@ -3,18 +3,12 @@
  **/
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 import { Footer, NavBar } from 'components';
-import config from 'meta/config';
 import 'styles/main.scss';
 
 export default function TemplateWrapper({ children }) {
   return (
     <div>
-      <Helmet>
-        <title>{config.siteTitle}</title>
-        <meta name="description" content={config.siteDescription} />
-      </Helmet>
       <NavBar />
       <div>{children()}</div>
       <Footer />
