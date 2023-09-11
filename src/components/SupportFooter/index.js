828853f82a1e9d266c7c7b51e87a62becf745131
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2023-09-11 18:06:25 -0400

    minor updates

--- src/components/SupportFooter/index.js
+++ src/components/SupportFooter/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import { P1, LinkHelper as Link, } from 'components';
 import {
@@ -9,6 +9,14 @@ import smileyFace from './svgs/smileyFace.svg';
 import unsmileyFace from './svgs/unsmileyFace.svg';
 
 function SupportFooter() {
+  const [feedback, setFeedback] = useState(null);
+
+  const handleFeedbackClick = (isPositive) => {
+    if (feedback === null) { // Ensure feedback has not been given before
+      setFeedback(isPositive);
+    }
+  };
+
   return (
     <div className="support-footer__container">
       <p className="support-footer__title">Help and Support</p>
@@ -16,16 +24,28 @@ function SupportFooter() {
         <div>
           <p className="support-footer__sub-title">Did this page help you?</p>
           <div className="feedback-icons">
-            <img id="postive-feedback" src={smileyFace} alt="Positive Feedback" className="icon"/>
-            <img id="postive-feedback" src={unsmileyFace} alt="Negative Feedback" className="icon"/>
+            <img 
+            onClick={() => handleFeedbackClick(true)} 
+            src={smileyFace} 
+            alt="Positive Feedback" 
+            className={`icon ${feedback === true ? 'icon-inverted' : ''}`}
+            id="postive-feedback"
+            />
+            <img
+            onClick={() => handleFeedbackClick(false)} 
+            src={unsmileyFace} 
+            alt="Negative Feedback" 
+            className={`icon ${feedback === false ? 'icon-inverted' : ''}`}
+            id="negative-feedback"
+            />
           </div>
         </div>
         <div>
           <p className="support-footer__sub-title">Help make our docs better</p>
             <ul>
             <li><a href="https://github.com/overture-stack/website/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title=BUG+-+" target="_blank" rel="noopener noreferrer">Submit an issue</a></li>
-            <li><a href="https://github.com/overture-stack/website/issues/new?assignees=&labels=new-feature&projects=&template=Feature_Request.md&title=Feature+Request+" target="_blank" rel="noopener noreferrer">Submit a feature request,</a></li>
-            <li><a href="https://github.com/overture-stack/website/blob/develop/CONTRIBUTING.md" target="_blank" rel="noopener noreferrer">Submit a pull request.</a></li>
+            <li><a href="https://github.com/overture-stack/website/issues/new?assignees=&labels=new-feature&projects=&template=Feature_Request.md&title=Feature+Request+" target="_blank" rel="noopener noreferrer">Submit a feature request</a></li>
+            <li><a href="https://github.com/overture-stack/website/blob/develop/CONTRIBUTING.md" target="_blank" rel="noopener noreferrer">Make a contribution</a></li>
             </ul>
         </div>
         <div>
