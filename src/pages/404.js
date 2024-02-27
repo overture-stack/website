a5c43e3cd14713ecbaf45de52ca84a30fa19fb13
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-02-26 20:07:14 -0500

    fixed modile sizing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { HOME_PATH } from 'constants/pages';
-import '../styles/main.scss';
+import { HOME_PATH } from '../../constants/pages';
 
 export default function NotFoundPage() {
   return (
     <div>
-      <section className="PageNotFound">
+      <section className="hero is-primary">
         <div className="hero-body">
           <div className="container">
             <div className="columns">
-              <div className="column is-offset-1">
+              <div className="column is-10 is-offset-1">
                 <div className="section is-centered">
                   <h1 className="title">404: Page Not Found</h1>
                   <h2 className="subtitle">Oops! The page you're looking for doesn't exist.</h2>
