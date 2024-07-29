1e1676117b1a263081b1534eca520f4d43800d90
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-07-29 10:20:20 -0400

    Menu Fix documentation

--- src/components/SectionTableOfContents/index.js
+++ src/components/SectionTableOfContents/index.js
@@ -3,31 +3,42 @@ import { useSSRWorkaround } from 'hooks';
 import { Icon, LinkHelper as Link } from 'components';
 import './styles.scss';
 
+// Makes URL for our documentation pages
 const makeUrl = (url) => `/documentation/${url}/`;
 
-// Custom hook for menu item logic
+// Custom Hook that manages the state and logic of the menu items, also handles opening and closing of menu items
 const useMenuItem = (page, path) => {
+  // Initialize isOpen state as undefined (null)
   const [isOpen, setIsOpen] = useState(null);
+  // Destructure propeties from the page object being passed (represented in the contents.yaml within the markdown folder)
   const { isHeading, pages: subpages, title, url: pageUrl } = page;
+  // Generate the URL for the current menu item using the makeURL function
   const url = makeUrl(pageUrl);
+  // isLinkActive will be set to the path that matches the URL
   const isLinkActive = path === url;
-
+  // If the menu item has subpages and is not just a heading inable toggling
   const hasToggle = !!(subpages && !isHeading);
+  // Check if the menu item should be active based on its toggle ability and path
   const isMenuActive = hasToggle && path.includes(url);
 
+  // Logic dictating if the menu should be open on initial load
   const isMenuOpenOnLoad = useMemo(
     () => hasToggle && isOpen === null && isMenuActive,
     [hasToggle, isOpen, isMenuActive]
   );
+
+  // Logic dicating if the menu should be open based on the toggle state and intial load state
   const isMenuOpen = useMemo(
     () => hasToggle && (isMenuOpenOnLoad || isOpen),
     [hasToggle, isMenuOpenOnLoad, isOpen]
   );
 
+  // Memoize the toggle function to prevent unnecessary re-renders unless dependencies change
   const toggle = useCallback(() => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   }, []);
 
+  // Automatically open the menu if it's active when the component mounts or updates
   useLayoutEffect(() => {
     if (isMenuActive) setIsOpen(true);
   }, [isMenuActive]);
@@ -45,6 +56,8 @@ const useMenuItem = (page, path) => {
   };
 };
 
+// Component that renders a single menu item using the custom useMenuItem hook for its logic
+// Different elements are rendered based on the item type (heading, link, or toggle)
 const MenuItem = ({ page, path, level }) => {
   const {
     isHeading,
@@ -91,6 +104,7 @@ const MenuItem = ({ page, path, level }) => {
   );
 };
 
+// Component that reders our list of menu items, also handles nesting of menu items
 const MenuItems = ({ level = 1, pages = [], path }) => {
   if (!pages.length) return null;
   const olClassName = `menu-level-${level}`;
@@ -104,6 +118,8 @@ const MenuItems = ({ level = 1, pages = [], path }) => {
   );
 };
 
+// This is the main component that wraps everything
+//    Uses useSSRWorkaround hook as a work around for server-side rendering issues related to gatsby
 export default function SectionTableOfContents({ pages, path }) {
   const { key } = useSSRWorkaround();
   return (
